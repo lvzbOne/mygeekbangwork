067e27499a0ff839f4e24d98f772ec6cc83d8979
lvzb <lvzb31988@163.com>
2022-04-22 09:45:45 +0800

    文件内容format格式修改

--- src/main/java/week6/question_6/goods.sql
+++ src/main/java/week6/question_6/goods.sql
@@ -11,76 +11,29 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 10/04/2022 23:09:18
+ Date: 22/04/2022 09:41:25
 */
 
 SET NAMES utf8mb4;
-SET
-FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for goods
 -- ----------------------------
 -- DROP TABLE IF EXISTS `goods`;
-CREATE TABLE IF NOT EXISTS `goods`
-(
-    `id` bigint
-(
-    20
-) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '商品物理id自增',
-    `goods_code` varchar
-(
-    32
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品编码',
-    `goods_name` varchar
-(
-    256
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品名称',
-    `label` varchar
-(
-    3
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '商品标签',
-    `price` decimal
-(
-    12,
-    4
-) NOT NULL DEFAULT 0.0000 COMMENT '商品价格',
-    `description` varchar
-(
-    1024
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品详情',
-    `listing_date` int
-(
-    8
-) NULL DEFAULT NULL COMMENT '上市日期',
-    `is_effective` char
-(
-    1
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '1' COMMENT '是否有效 1:有效,2:无效',
-    `gmt_create` datetime
-(
-    0
-) NULL DEFAULT CURRENT_TIMESTAMP
-(
-    0
-) COMMENT '创建时间',
-    `gmt_modified` datetime
-(
-    0
-) NULL DEFAULT CURRENT_TIMESTAMP
-(
-    0
-) COMMENT '修改时间',
-    PRIMARY KEY
-(
-    `id`
-) USING BTREE,
-    UNIQUE INDEX `goods_code`
-(
-    `goods_code`
-)
-  USING BTREE
-    ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '商品表' ROW_FORMAT = Dynamic;
+CREATE TABLE IF NOT EXISTS `goods`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '商品物理id自增',
+  `goods_code` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品编码',
+  `goods_name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品名称',
+  `label` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '商品标签',
+  `price` decimal(12, 4) NOT NULL DEFAULT 0.0000 COMMENT '商品价格',
+  `description` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品详情',
+  `listing_date` int(8) NULL DEFAULT NULL COMMENT '上市日期',
+  `is_effective` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '1' COMMENT '是否有效 1:有效,2:无效',
+  `gmt_create` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `gmt_modified` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `goods_code`(`goods_code`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '商品表' ROW_FORMAT = Dynamic;
 
-SET
-FOREIGN_KEY_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = 1;
--- src/main/java/week6/question_6/orders.sql
+++ src/main/java/week6/question_6/orders.sql
@@ -11,91 +11,32 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 10/04/2022 23:09:31
+ Date: 22/04/2022 09:41:34
 */
 
 SET NAMES utf8mb4;
-SET
-FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for orders
 -- ----------------------------
 -- DROP TABLE IF EXISTS `orders`;
-CREATE TABLE IF NOT EXISTS `orders`
-(
-    `id` bigint
-(
-    20
-) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '订单物理id自增',
-    `order_code` varchar
-(
-    32
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '订单编码',
-    `shop_code` varchar
-(
-    32
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '店铺编码',
-    `goods_code` varchar
-(
-    32
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品编码',
-    `identify_card` varchar
-(
-    18
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '身份证号码',
-    `phone` varchar
-(
-    32
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '手机号',
-    `price` decimal
-(
-    12,
-    4
-) NOT NULL DEFAULT 0.0000 COMMENT '商品价格',
-    `goods_num` int
-(
-    5
-) NOT NULL DEFAULT 1 COMMENT '商品购买数量',
-    `remark` varchar
-(
-    256
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
-    `is_effective` char
-(
-    1
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '1' COMMENT '订单状态 1:有效,2:无效',
-    `store_date` int
-(
-    8
-) NOT NULL COMMENT '订单生成日期格式yyyyMMdd',
-    `gmt_create` datetime
-(
-    0
-) NULL DEFAULT CURRENT_TIMESTAMP
-(
-    0
-) COMMENT '创建时间',
-    `gmt_modified` datetime
-(
-    0
-) NULL DEFAULT CURRENT_TIMESTAMP
-(
-    0
-) COMMENT '修改时间',
-    PRIMARY KEY
-(
-    `id`
-) USING BTREE,
-    UNIQUE INDEX `uk_o_s_g_i`
-(
-    `order_code`,
-    `shop_code`,
-    `goods_code`,
-    `identify_card`
-)
-  USING BTREE
-    ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '订单表' ROW_FORMAT = Dynamic;
+CREATE TABLE IF NOT EXISTS `orders`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '订单物理id自增',
+  `order_code` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '订单编码',
+  `shop_code` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '店铺编码',
+  `goods_code` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品编码',
+  `identify_card` varchar(18) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '身份证号码',
+  `phone` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '手机号',
+  `price` decimal(12, 4) NOT NULL DEFAULT 0.0000 COMMENT '商品价格',
+  `goods_num` int(5) NOT NULL DEFAULT 1 COMMENT '商品购买数量',
+  `remark` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
+  `is_effective` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '1' COMMENT '订单状态 1:有效,2:无效',
+  `store_date` int(8) NOT NULL COMMENT '订单生成日期格式yyyyMMdd',
+  `gmt_create` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `gmt_modified` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uk_o_s_g_i`(`order_code`, `shop_code`, `goods_code`, `identify_card`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '订单表' ROW_FORMAT = Dynamic;
 
-SET
-FOREIGN_KEY_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = 1;
